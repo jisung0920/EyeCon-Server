7b6787d17c004979f0ad838e40344c1d43b3b56d
jisung <jisung0920@gmail.com>
2020-06-02 09:29:29 +0900

    face+eye tracking model

---
+++ econ.py
@@ -0,0 +1,120 @@
+import socket
+import numpy as np
+import cv2
+from scipy.spatial import distance as dist
+from imutils import face_utils
+import imutils
+import dlib
+import random
+import os
+import torch
+
+
+
+
+
+def getCascade(util_path) :
+
+    face_haar_path = util_path + 'haarcascade_frontalface_default.xml'
+    eye_haar_path  = util_path + 'haarcascade_eye.xml'
+    
+    faceCascade = cv2.CascadeClassifier(face_haar_path)
+    eyeCascade = cv2.CascadeClassifier(eye_haar_path)
+    
+    return faceCascade,eyeCascade  
+
+
+def getFaceEyePoint(frame,faceCascade, eyeCascade) :
+    
+    faces = faceCascade.detectMultiScale(frame,1.2,cv2.COLOR_BGR2GRAY)
+    faceFrame = frame
+    for (x,y,w,h) in faces :
+    	faceFrame = frame[y:y+h,x:x+w]
+    eyes=eyeCascade.detectMultiScale(faceFrame,1.2,cv2.COLOR_BGR2GRAY)
+    
+    return faces,eyes
+
+
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    C = dist.euclidean(eye[0], eye[3])
+    return (A + B) / (2.0 * C)
+
+        
+def frameBlinkChecker(frame,predictor,detector,blink_th,lStart,lEnd) :
+
+
+	frame = imutils.resize(frame, width=450)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	rects = detector(gray, 0)
+	EAR = 0
+	for rect in rects:
+	    shape = predictor(gray, rect)
+	    shape = face_utils.shape_to_np(shape)
+	    leftEye = shape[lStart:lEnd]
+	    EAR = eye_aspect_ratio(leftEye)
+	
+	if EAR > blink_th:
+		return True
+	else :
+		return False
+
+
+def getGazeXY(model,image):
+    tmp = model(image[None, ...])[0]
+    x = tmp[0].item()
+    y = tmp[1].item()
+    return x,y
+
+def getFaceXY(faces) :
+    face = faces[0]
+    x = face[0] + face[2]/2
+    y = face[1] + face[3]/2
+    return [x,y]
+
+def getXY(cur_point,next_point,count,diff_TH,freeze_TH,momentum=0.9) :
+    
+    if(count == -1) :
+        count=0
+        return next_point
+
+    point_dff = dist.euclidean(cur_point,next_point)
+    
+    if(point_dff >diff_TH) :
+        count +=1
+        if(count>freeze_TH) :
+            count = 0
+            return next_point
+        return cur_point
+    count  = 0
+    return (next_point * momentum) + (cur_point* (1-momentum))
+        
+def recvAll(sock, count):
+    buf = b''
+    while count:
+        # count만큼의 byte를 읽어온다. socket이 이미지 파일의 bytes를 한번에 다 못 읽어오므로
+        newbuf = sock.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
+
+def load_checkpoint(filename='./checkpoint.pth.tar'):
+    print(filename)
+    if not os.path.isfile(filename):
+        print('Cant load')
+        return None
+    
+    device = torch.device('cpu')
+    state = torch.load(filename,map_location=device)
+    return state
+
+def strechingPoint(x,y,beforeW,beforeH,afterW,afterH,strechFactor=1.2):
+    return x*(afterW/beforeW) *strechFactor , y* (afterH/beforeH) *strechFactor
+
+# Model
+# def getGazeXY(image,face,eyes) : 
+# 	x = 500 + random.randint(-50, 50)
+# 	y = 800 + random.randint(-50, 50)
+# 	return x,y
---
+++ run.py
@@ -0,0 +1,133 @@
+import argparse
+import econ
+import socket
+import numpy as np
+import cv2
+from scipy.spatial import distance as dist
+from imutils import face_utils
+import dlib
+import vggModel
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+
+diff_TH = 200
+freeze_TH = 5
+WEIGHT_POINT = 0.8
+
+IMG_WIDTH = 480
+IMG_HEIGHT = 640
+ANRD_WIDTH = 1170
+ANRD_HEIGHT = 1780
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--util_path',default='./utils/',required = False, help='util file path')
+parser.add_argument('--blink_th',default=0.3,required=False, help='eye blick value threshold')
+parser.add_argument('--port',default=8100,type=int)
+parser.add_argument('--ip',default='192.168.0.25')
+
+args = parser.parse_args()
+
+util_path  = args.util_path
+IP,PORT = args.ip, args.port
+BLINK_TH = args.blink_th
+
+
+faceCascade, eyeCascade = econ.getCascade(util_path)
+
+predictor_path = util_path + 'shape_predictor_68_face_landmarks.dat'
+
+predictor = dlib.shape_predictor(predictor_path)
+detector = dlib.get_frontal_face_detector()
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+
+
+model = vggModel.VGG_16() 
+
+
+device = torch.device('cpu')
+checkpoint =torch.load('./checkpoint.pth.tar',map_location=device)['state_dict']
+for key in list(checkpoint.keys()):
+    if 'module.' in key:
+        checkpoint[key.replace('module.', '')] = checkpoint[key]
+        del checkpoint[key]
+
+model.load_state_dict(checkpoint)
+
+
+transformImg = transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.ToTensor()])
+
+
+print(IP,':',PORT)
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((IP, PORT))
+server_socket.listen()
+print('listening...')
+
+serverOn = True
+count = -1
+cur_point = np.array([600,870])
+x = 600
+y= 860
+count = -1
+try:
+    client_socket, addr = server_socket.accept()
+    print('Connected with ', addr)
+
+
+        
+    while serverOn:
+
+        length = econ.recvAll(client_socket, 10)
+
+        frame_bytes = econ.recvAll(client_socket, int(length))
+
+        image = cv2.imdecode(np.frombuffer(frame_bytes, np.uint8), cv2.IMREAD_COLOR)
+        print('Get image from Android')
+        inputData = Image.fromarray(image) 
+        inputData = transformImg(inputData)
+        inputData = torch.autograd.Variable(inputData, requires_grad = True)
+
+        faceFrame,eyeFrame = econ.getFaceEyePoint(image,faceCascade,eyeCascade)
+
+        gazePoint = np.array( econ.getGazeXY(model,inputData) )
+
+        if len(faceFrame) > 0 :
+            facePoint = np.array(econ.getFaceXY(faceFrame))
+        else :
+            facePoint = cur_point
+
+
+        if(econ.frameBlinkChecker(image,predictor,detector,BLINK_TH,lStart,lEnd)) :
+            blink = 0
+        else :
+            blink = 1
+
+        # blink = 0
+        # x = round(x*1200/2  + 1200/2)
+        # y = round(y*1740/2  + 1740/2)
+        x, y  = econ.getXY(cur_point, facePoint,count,diff_TH,freeze_TH)
+        x, y  = econ.strechingPoint(x,y,IMG_WIDTH,IMG_HEIGHT,ANRD_WIDTH,ANRD_HEIGHT)
+        cord = str(int(x)) + '/' + str(int(y)) + '/' +str(blink)
+        print('Send to Android :',cord)
+        client_socket.sendall(bytes(cord,'utf8'))
+
+
+        # cv2.imshow('AndroidScreen', image)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+        	break
+
+
+except Exception as e:
+    print(e)
+    print('Connecting Error')
+    pass
+finally:
+    print('End of the session')
+
+
